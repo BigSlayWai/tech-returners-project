4490fddb801474b7582117c9649b0644c01a50e6
Wai Hleng Swe <waihlengpyae@gmail.com>
2025-10-09 20:38:52 +0100

    fixed next config file

--- events-platform/next.config.js
+++ events-platform/next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true, // Enable Server Actions
+  },
   images: {
     domains: ['utfs.io'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'utfs.io',
-        port: ''
-      }
-    ]
-  }
-}
+        port: '',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
