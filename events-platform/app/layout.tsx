c81e525b61caacc565611674ff3d0fdf6361f308
Wai Hleng Swe <waihlengpyae@gmail.com>
2025-10-09 11:31:08 +0100

    feat!: added sign in sign up

--- events-platform/app/layout.tsx
+++ events-platform/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 import './globals.css'
 
@@ -8,6 +16,7 @@ const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
   variable: '--font-poppins',
 })
+
 
 export const metadata: Metadata = {
   title: 'Tech Returners',
@@ -19,12 +28,29 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${poppins.variable} antialiased`}>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
---
+++ events-platform/components/shared/Header.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+        <div className="wrapper mx-auto flex h-16 items-center justify-between">
+            <Link href="/" className="w-36 text-lg font-bold">
+            <Image 
+            src="/assets/images/logo.svg" alt="logo" width={144} height={32} 
+            alt="Tech Returners Events Platform Logo"
+            />
+            </Link>
+            <div className="flex w-32 justify-end gap-3">
+            
+            </div>
+        </div>
+        </header>
+  )
+}
+
+export default Header
