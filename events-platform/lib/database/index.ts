4f429c9564ac22c85ce50cce676855ff631b0f2b
Wai Hleng Swe <waihlengpyae@gmail.com>
2025-10-09 17:30:50 +0100

    feat!: initalising backend

--- events-platform/lib/database/index.ts
+++ events-platform/lib/database/index.ts
@@ -2,23 +2,19 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  if (cached.conn) return cached.conn;
+
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'techreturners',
+    dbName: 'evently',
     bufferCommands: false,
   })
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-};
+}
