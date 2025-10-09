c174705a05432a751807ba26b186885fb125e337
Wai Hleng Swe <waihlengpyae@gmail.com>
2025-10-09 11:44:11 +0100

    fix!: fixed sign in button

--- events-platform/app/layout.tsx
+++ events-platform/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 
 import './globals.css'
 
@@ -28,28 +21,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${poppins.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
-          {children}
-        </body>
+        <body className={poppins.variable}>{children}</body>
       </html>
     </ClerkProvider>
   )
--- events-platform/components/shared/Header.tsx
+++ events-platform/components/shared/Header.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
+import { Button } from '../ui/button'
+import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs"
 
 const Header = () => {
   return (
@@ -13,7 +15,13 @@ const Header = () => {
             />
             </Link>
             <div className="flex w-32 justify-end gap-3">
-            
+            <SignedOut>
+              <SignInButton>
+                <Button className="rounded-full" size="lg">
+                  Sign In
+                </Button>
+              </SignInButton>
+            </SignedOut>
             </div>
         </div>
         </header>
