cc9fe667c4e8d30b3e2ef18cefb70fbd00a057d9
Wai Hleng Swe <waihlengpyae@gmail.com>
2025-10-19 08:36:11 +0100

    fix

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,8 @@ export const config = {
     '/',
     '/events/:id',
     '/api/webhook/clerk',
-    '/api/webhook/stripe',
     '/api/uploadthing',
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    '/((?!_next|api/webhook/stripe|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)((?!webhook/stripe).*)',
   ],
 };
